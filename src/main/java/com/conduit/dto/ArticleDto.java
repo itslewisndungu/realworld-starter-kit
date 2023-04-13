d1857f5f1346bc6e114821eac5f84746fbe6d24b
itslewisndungu <itslewisndungu@gmail.com>
2023-04-13 23:15:54 +0300

    format: Remove unused imports

--- src/main/java/com/conduit/dto/ArticleDto.java
+++ src/main/java/com/conduit/dto/ArticleDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Setter
