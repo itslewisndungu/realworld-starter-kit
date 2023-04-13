86f7b9aeb671945032ce763c99046ae2bd55e1da
itslewisndungu <itslewisndungu@gmail.com>
2023-04-13 22:29:57 +0300

    feat: Retrieve article by slug
    Add the method to retrieve the articles by their slugs to the `ArticleRepository`

--- src/main/java/com/conduit/repositories/ArticleRepository.java
+++ src/main/java/com/conduit/repositories/ArticleRepository.java
@@ -4,6 +4,9 @@ import com.conduit.models.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    Optional<Article> getArticleBySlug(String slug);
 }
