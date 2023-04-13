d80a6be4ba4e297ac4717ed90dbb5ec185b6d682
itslewisndungu <itslewisndungu@gmail.com>
2023-04-13 21:42:48 +0300

    Create an `Article` repository

---
+++ src/main/java/com/conduit/models/repositories/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.conduit.models.repositories;
+
+import com.conduit.models.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
