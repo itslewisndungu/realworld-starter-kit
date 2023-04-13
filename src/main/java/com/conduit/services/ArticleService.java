eac5bdb40baacbb400ec120bc78f863f9a50c2dc
itslewisndungu <itslewisndungu@gmail.com>
2023-04-13 23:02:09 +0300

    feat: Create a service layer for the article

---
+++ src/main/java/com/conduit/services/ArticleService.java
@@ -0,0 +1,57 @@
+package com.conduit.services;
+
+import com.conduit.dto.ArticleDto;
+import com.conduit.models.Article;
+import com.conduit.repositories.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository repository;
+
+    public List<Article> retrieveAllArticles() {
+        return this.repository.findAll();
+    }
+
+    public Article getArticle(String slug) {
+        return this.repository.getArticleBySlug(slug).orElseThrow(
+                () -> new NoSuchElementException(String.format("Article with the slug %s not found", slug))
+        );
+    }
+
+    public Article createNewArticle(ArticleDto article) {
+        Article newArticle = Article
+                .builder()
+                .slug(article.getSlug())
+                .title(article.getTitle())
+                .description(article.getDescription())
+                .body(article.getBody())
+                .build();
+
+        return this.repository.save(newArticle);
+    }
+
+    public Article updateArticle(ArticleDto article, String slug) {
+        Article oldArticle = this.repository.getArticleBySlug(slug).orElseThrow(
+                () -> new NoSuchElementException(String.format("Article with the slug %s not found", slug))
+        );
+
+        oldArticle.setTitle(article.getTitle());
+        oldArticle.setBody(article.getBody());
+        oldArticle.setDescription(article.getDescription());
+
+        return this.repository.save(oldArticle);
+    }
+
+    public void deleteArticle(String slug) {
+        Article article = this.repository.getArticleBySlug(slug).orElseThrow(
+                () -> new NoSuchElementException(String.format("Article with the slug %s not found", slug))
+        );
+        this.repository.delete(article);
+    }
+}
