b74ef4796343a41c084b4414a8675c426e6d72b4
itslewisndungu <itslewisndungu@gmail.com>
2023-04-20 17:19:49 +0300

    Enable auditing for the `Article` entity

--- src/main/java/com/conduit/domain/content/Article.java
+++ src/main/java/com/conduit/domain/content/Article.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue
