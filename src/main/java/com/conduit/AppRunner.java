7eedad1ed27dd6a4d5c262625c8e98e717ef4d31
itslewisndungu <itslewisndungu@gmail.com>
2023-04-22 16:02:07 +0300

    Generate sample tags for the app startup

--- src/main/java/com/conduit/AppRunner.java
+++ src/main/java/com/conduit/AppRunner.java
@@ -2,12 +2,16 @@ package com.conduit;
 
 import com.conduit.domain.content.Article;
 import com.conduit.domain.content.ArticleRepository;
+Vimport com.conduit.domain.content.Tag;
+import com.conduit.domain.content.TagRepository;
 import com.conduit.domain.user.User;
 import com.conduit.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 
 @Component
@@ -16,6 +20,7 @@ public class AppRunner implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final ArticleRepository articleRepository;
+    private final TagRepository tagRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -48,19 +53,27 @@ public class AppRunner implements CommandLineRunner {
     }
 
     private void generateArticle(User author) {
+        var tag1 = new Tag().name("react");
+        var tag2 = new Tag().name("angular");
+
+        tagRepository.save(tag1);
+        tagRepository.save(tag2);
+
         var article1 = new Article()
                 .author(author)
                 .slug("hello-world")
                 .title("New World")
                 .description("New world generated article")
-                .body("This is the new generated article");
+                .body("This is the new generated article")
+                .tags(Set.of(tag1));
 
         var article2 = new Article()
                 .author(author)
                 .slug("new-article")
                 .title("New Article")
                 .description("New generated article")
-                .body("This is the generated article");
+                .body("This is the generated article")
+                .tags(Set.of(tag2));
 
 
         articleRepository.save(article1);
